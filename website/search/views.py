a0fe400ea337fd2be6a6b8b2e4e0b000e0a0fb16
@MrsLecter <54142283+MrsLecter@users.noreply.github.com>
2022-06-18 23:29:12 +0300

    Update views.py

--- website/search/views.py
+++ website/search/views.py
@@ -4,38 +4,7 @@ import re
 from numpy import integer
 import website.data_access
 
-def update_search_category(filter_goods, get_category):
-    if get_category is not None:
-        filter_goods.update({
-            'category': get_category
-        })
 
-
-def make_request(filter_goods, price_from, price_to):
-    """
-        > Greater than:   x > y
-        < Less than:	x < y
-        """
-    if len(price_from) != 0 and len(price_to) != 0:
-        filter_goods.update(
-            {
-                "$and": [
-                    {'price': {'$gt': int(price_from)}},
-                    {'price': {'$lt': int(price_to)}}
-                ]
-            }
-        )
-    elif len(price_from) == 0 and len(price_to) != 0:
-        filter_goods.update(
-            {'price': {'$lt': int(price_to)}}
-        )
-
-    elif len(price_from) != 0 and len(price_to) == 0:
-        filter_goods.update(
-            {'price': {'$gt': int(price_from)}}
-        )
-
-    return filter_goods
 
 def search(request):
     keyword = request.GET.get('keyword', '')
@@ -68,10 +37,9 @@ def search(request):
     #     make_request(filter_obj, price_from, price_to)
 
     # if category != "all_categories":
-    #     update_search_category(filter_obj, category)
+    #     update_category(filter_obj, category)
 
-    # filtered_goods = website.data_access.getAllObject("goods")
+    # filtered_goods = getAllObject("goods")
 
     # items = website.data_access.getAllObject('category')
     return render(request, "search/search.html", {"category": unique_category, "goods": goods_current_category, "price_min": price_min, "price_max": price_max})
-    # return render(request, "search/search.html", {"filter_good": filtered_goods, "keyword": keyword, "c_name": category, "category": items})
